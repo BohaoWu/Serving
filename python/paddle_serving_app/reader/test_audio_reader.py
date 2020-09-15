aa9ea5abd2938032e9ae7748f3c334ec4f23248f
BohaoWu <gxshzy@outlook.com>
2020-09-15 11:41:43 +0000

    Selected New files.

--- python/paddle_serving_app/reader/test_audio_reader.py
+++
@@ -1,6 +0,0 @@
-from audio_reader import AudioFeatureOp 
-
-if __name__ == '__main__':
-    local_video_path = 'case.mp4'
-    test_op = AudioFeatureOp()
-    test_op.extract_audio_from_video(local_video_path)
